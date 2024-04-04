79712262eac59b3c4ad1dbf00f1a1238663c5532
Pi <paingthetko.pi@gmail.com>
2024-04-04 13:39:43 +0400

    Update inclass.ino

--- inclass.ino
+++ inclass.ino
@@ -33,3 +33,45 @@ void loop() {
 }
 
 // -------------------------------------------------------------------
+// 2. Non Blocking Edition
+
+const int yellowLED = 3;
+const int redLED = 5;
+const int potPin = A0;
+const int buttonPin = 7;
+
+unsigned long previousMillis = 0; // Stores the last time the LED was updated
+
+void setup() {
+  pinMode(yellowLED, OUTPUT);
+  pinMode(redLED, OUTPUT);
+  pinMode(potPin, INPUT);
+  pinMode(buttonPin, INPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  int potValue = analogRead(potPin);
+  int delayTime = map(potValue, 0, 1023, 100, 2000);
+  int buttonState = digitalRead(buttonPin);
+
+  Serial.print("Button state: ");
+  Serial.println(buttonState);
+
+  unsigned long currentMillis = millis();
+
+  if (currentMillis - previousMillis >= delayTime) {
+    // save the last time you blinked the LED
+    previousMillis = currentMillis;
+
+    // If the LEDs are on, turn them off and vice-versa
+    if (digitalRead(yellowLED) == LOW) {
+      digitalWrite(yellowLED, HIGH);
+      digitalWrite(redLED, LOW);
+    } else {
+      digitalWrite(yellowLED, LOW);
+      digitalWrite(redLED, HIGH);
+    }
+  }
+}
+
